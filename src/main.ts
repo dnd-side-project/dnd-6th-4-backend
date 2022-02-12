03ff983d3467abe0927ad3aba4da7187ad905401
lshn1007 <lshn1007@hanyang.ac.kr>
2022-02-12 20:46:22 +0900

    feat: swagger 미들웨어 추가
    {loopback}:3000/api 로 확인 가능

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
+	// swagger
+	const config = new DocumentBuilder()
+		.setTitle('Ping pong')
+		.setDescription('The ping pong API description')
+		.setVersion('0.0')
+		.addTag('1st')
+		.build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, document);
+
+
 	await app.listen(3000);
 }
 bootstrap();
