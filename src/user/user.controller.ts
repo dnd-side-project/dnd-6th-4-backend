538b9a1f1761fca44e53c67e9a024c397f141ca5
lshn1007 <lshn1007@hanyang.ac.kr>
2022-02-17 14:57:29 +0900

    feat: 로그인 도메인 초기구상 완료

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,19 +3,32 @@ import {
 	Get,
 	Post,
 	Body,
-	Patch,
-	Param,
-	Delete,
-    Query
+	// Patch,
+	Param
+	// Delete,
+	// Query
 } from '@nestjs/common';
 import { UserService } from './user.service';
-import { UserRequestDto } from './dto/user-request.dto';
+import { UserCreateDto } from './dto/user-create.dto';
+import { User } from './entities/user.model';
 
 @Controller('user')
 export class UserController {
 	constructor(private userService: UserService) {}
+
 	@Get('all')
 	findAll() {
 		return this.userService.findAll();
+	}
+
+	@Post()
+	async create(@Body() createUserDto: UserCreateDto): Promise<any> {
+		return await this.userService.create(createUserDto);
+		// return "reponse test finish";
+	}
+
+	@Get(':email')
+	findOne(@Param('email') email: string): Promise<User> {
+		return this.userService.findOne(email);
 	}
 }
